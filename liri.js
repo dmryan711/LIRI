9f48621bfd5646421706055e82d832363f48f4cc
Pickle Rick <dmryan711@gmail.com>
2019-03-19 19:04:54 -0400

    Additional framework - boilerplate app

--- liri.js
+++ liri.js
@@ -1,8 +1,11 @@
+//Imports
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-
-
 var spotify = new Spotify(keys.spotify);
 
-console.log(spotify);
+//Command line variables
+const CONCERT_THIS = "concert-this";
+const SPOTIFY_THIS_SONG = "spotify-this-song";
+const MOVIE_THIS = "movie-this";
+const DO_WHAT_IT_SAYS = "do-what-it-says";
